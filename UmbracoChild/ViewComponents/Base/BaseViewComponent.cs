2e286a6d218f7558acb1bacabb56a0ce94d554cd
Peter Sandvad Larsen <pels@semler.dk>
2026-05-01 08:01:28 +0200

    Adds ViewComponents and layout structure for block rendering in child project
    Introduces a generic base ViewComponent for Block Grid elements and implements specific components for article, headline, image, stage, and text blocks.
    
    Also adds corresponding component views, a shared layout, and ViewStart configuration to support structured rendering within the child project, along with Umbraco Forms tag helper registration.

---
+++ UmbracoChild/ViewComponents/Base/BaseViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+
+namespace UmbracoChild.ViewComponents.Base
+{
+    public abstract class BaseViewComponent<T> : ViewComponent where T : PublishedElementModel
+    {
+        protected readonly IUmbracoContextAccessor _umbracoContextAccessor;
+        protected readonly IPublishedValueFallback _publishedValueFallback;
+
+        protected BaseViewComponent(IUmbracoContextAccessor umbracoContextAccessor, IPublishedValueFallback publishedValueFallback)
+        {
+            _umbracoContextAccessor = umbracoContextAccessor;
+            _publishedValueFallback = publishedValueFallback;
+        }
+
+        public virtual IViewComponentResult CustomInvoke(IPublishedElement publishedElement)
+        {
+            T? model = Activator.CreateInstance(typeof(T), publishedElement, _publishedValueFallback) as T;
+            return View(model);
+        }
+
+        public IViewComponentResult Invoke(BlockGridItem publishedElement)
+        {
+            return CustomInvoke(publishedElement.Content);
+        }
+
+        //protected string GetViewPath(string alias)
+        //{
+        //    return alias;
+        //}
+    }
+}
